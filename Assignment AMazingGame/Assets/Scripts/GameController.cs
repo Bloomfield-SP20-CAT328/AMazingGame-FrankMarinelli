11b8d3c1f0a95b58d3f14874bc006995f6eddc7b
Frank <fmarinellicollege@gmail.com>
2020-04-07 19:57:13 -0400

    Commit for help

--- Assignment AMazingGame/Assets/Scripts/GameController.cs
+++ Assignment AMazingGame/Assets/Scripts/GameController.cs
@@ -6,23 +6,100 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    //Nothing here should cause problems. Go to MazeGen.
-    //I removed the int values.
-	public int mazeWidth;
-	public int mazeHeight;
-	public GameObject visibleMaze;
+
+	public int mazeWidth = 40;
+	public int mazeHeight = 40;
+	public float mazeComplexity = 0.75f;
+	public float mazeDensity = 0.75f;
+	public Text widthTextElement;
+	public Text heightTextElement;
+	public Slider complexitySliderUIElement;
+	public Slider densitySliderUIElement;
+	public Text complexityTextElement;
+	public Text densityTextElement;
+	Maze myMazeGen = new MazeGen();
+	GameObject visibleMaze;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		MazeGen mazeGenerate = new MazeGen();
-		mazeGenerate.GenerateMaze(mazeWidth, mazeHeight);
-		
+		UpdateDensityTextUI();
+		UpdateComplexityTextUI();
+		visibleMaze = new GameObject("VisibleMaze GameObject");
+		GenerateMaze();
+		DrawTheMaze();
+
 	}
 
-    public void RegenerateButton(string regenerateMaze)
+	public void RegenerateButton(string regenerateMaze)
 	{
 		SceneManager.LoadScene(regenerateMaze);
+	}
+
+	public void UpdateDensityTextUI()
+	{
+		densityTextElement.text = "Density: " + densitySliderUIElement.value.ToString("00.00");
+	}
+
+	public void UpdateComplexityTextUI()
+	{
+		complexityTextElement.text = "Complexity: " + complexitySliderUIElement.value.ToString("00.00");
+	}
+
+	public void GenerateMaze_ClickHandler()
+	{
+		if (widthTextElement.text == "" || heightTextElement.text == "")
+		{
+			return;
+		}
+		int width = int.Parse(widthTextElement.text);
+		int height = int.Parse(heightTextElement.text);
+		float complexity = complexitySliderUIElement.value;
+		float density = densitySliderUIElement.value;
+		mazeWidth = width;
+		mazeHeight = height;
+		mazeComplexity = compelxity;
+		mazeDensity = density;
+		ClearMaze();
+		GenerateMaze();
+		DrawTheMaze();
+	}
+
+	protected void GenerateMaze()
+	{
+		myMazeGen.GenerateMaze(mazeWidth, mazeHeight, mazeComplexity, mazeDensity);
+
+	}
+
+	protected void DrawTheMaze()
+	{
+		//Warning, the colors I chose for this portion of the code might make your eyes vomit. I take no responsibility if your eyes vomit from my colors.
+		GameObject mazeFloor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		mazeFloor.transform.position = new Vector3(myMazeGen.Width / 2, 0, myMazeGen.Height / -2);
+		mazeFloor.transform.localScale = new Vector3(myMazeGen.Width / 10.0f, 1.0f, myMazeGen.Height / 10.0f);
+		mazeFloor.transform.parent = visibleMaze.transform;
+		mazeFloor.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f);
+		for (int y = 0; y < myMazeGen.Height; y++)
+		{
+			for (int x = 0; x < myMazeGen.Width; x++)
+			{
+				if (myMazeGen.IsAWall(x, y))
+				{
+					GameObject mazeCube = GameObject.GetPrimitive(PrimitiveType.Cube);
+					mazeCube.transform.position = new Vector3(x, 0.5f, y * -1);
+					mazeCube.transform.localScale = new Vector3(1, 1, 1);
+					mazeCube.transform.parent = visibleMaze.transform;
+					mazeCube.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 1.0f);
+				}
+			}
+		}
+		float cameraY = (myMazeGen.Width < myMazeGen.Height) ? myMazeGen.Height + 2 : myMazeGen.Width + 2;
+		Camera.main.transform.position = new Vector3(myMazeGen.Width / 2.0f, cameraY, -1 * myMazeGen.Height / 2.0f);
+	}
+	protected void ClearMaze()
+	{
+		Destroy(visibleMaze);
+		visibleMaze = new GameObject("VisibleMaze GameObject");
 	}
 
 }
--- Assignment AMazingGame/Assets/Scripts/MazeGen.cs
+++ Assignment AMazingGame/Assets/Scripts/MazeGen.cs
@@ -1,168 +1,139 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MazeGen
 {
-//Ill be documenting my code from now on with this assignment. Ill be labeling concerns, stresses, anger, headaches and tears. Mainly stresses and anger.
-//Documentation will tell changes I tried making with the code, if it helped or not, as well as what changing the code did for the generation.
-//As a result, this script has been the largest headache to try and tackle. Based on the tinkering I did, I was able to get much faster load times, and varying generation, but also errors with each generation.
-//Whatever is happening with this code to cause errors and weirdness might cause the UI portion of this Assignment(Assignment 13) to not work properly, despite being hooked up hopefully correctly.
-//With that said, I expect to get ripped apart for having buggy, messy code. If that happens, Hello Class! This is Frank from 2:04pm.
-	struct Point
+   struct Point
 	{
 		public int x, y;
-		public Point(int ptX, int ptY)
+        public Point(int ptX, int ptY)
 		{
 			x = ptX;
 			y = ptY;
 		}
+
+		public override string ToString()
+		{
+			return "(" + x + "," + y + ")";
+		}
 	}
-    //Lines 23-42 should not be causing any issues.
+
+	//Under the advice of TJ and Prof. Pollati, I ditched the old buggy, crappy script and tried something different.
+	//This is the backtracking generator maze with no recursion. I looked at Pollati's code to help me out with some of this stuff.
+	//Theres a good chance something is broke, because... well.... bugbugbugbugbugbugbugbugbug. I'm also not the best coder. That's why the world has Carlos.
+	//This will most likely stay recursion free... unless I don't like the results of doing the Backtracking w/o recursion way. However, as long as its different from my old crappy code. Ill be happy.
+	//Some values have changed. I hope this didn't brake anything.
 	private byte[,] maze;
 
 	private int width = 0;
-	public int Width { get { return width; } }
-
+    private int Width { get { return width; } }
 	private int height = 0;
-	public int Height { get { return height; } }
+    private int Height { get { return height; } }
 
-	private float complexity = 0.75f;
-	public float Complexity { get { return complexity; } }
 
-	private float density = 0.75f;
-	public float Density { get { return density; } }
-
-	//public GameObject mazeCubes;
-
+	private List<Point> track = new List<Point>();
 
 	public MazeGen() { }
+	
 
 	public void GenerateMaze(int mazeWidth, int mazeHeight)
 	{
-		GenerateMaze(mazeWidth, mazeHeight, complexity, density);
+		width = (int)(mazeWidth / 2) * 2 + 1;
+		height = (int)(mazeHeight / 2) * 2 + 1;
+		//This code is likely to change. It also gives me nightmares, cuz its probably what broke my previous project
 
-	}
-    //I edited some values here. Seemed to have helped with the generation. Made it more stable.
-	public void GenerateMaze(int mazeWidth, int mazeHeight, float mazeComplexity, float mazeDensity)
-	{
-		complexity = mazeComplexity;
-		density = mazeDensity;
-		width = (int)(mazeWidth / 2) * 3 + 1;
-		height = (int)(mazeHeight / 2) * 3 + 1;
-		complexity = (int)(complexity * 3 * (width + height));
-		density = (float)Mathf.Floor(density * (int)(height / 2) * (int)(width / 2));
-		maze = new byte[width, height];
-		for (int y = 0; y < height; y++)
+		maze = new byte[width.height];
+
+        for(int j=0; j<height; j++)
 		{
-			for (int x = 0; x < width; x++)
+            for(int i=0; i<width; i++)
 			{
-				
-				if ((y == 0) || (y == (height - 1)) || (x == 0) || (x == (width - 1)))
-				{
-					maze[x, y] = 1;
-				}
-				else
-				{
-					maze[x, y] = 0;
-				}
-			}
-		}
-        //I feel like something here is giving me the most problems. Im not sure why or what. This version of the code gives on average 3 errors(no generation), 3 successes(generation).
-        //The successes range from 414 messages in console, 690 messages in the console, 828 messages in the console, and 999+ messages in the console. You can tell which is which by load times.
-        //414 messages takes around 2-4 seconds to load. 690 messages take around 4-6 second to load. 828 takes around 5-8 seconds to load. 999+ messages take 7-9 seconds to load. Errors load instantly. MUCH better than the 1-2 minutes it took before.
-        //The error it always gives me is IndexOutOfRangeException: Index was outside the bounds of the array.
-        for (int i = 0; i < density; i++)
-		{
-			var x = (int)(Random.Range(0, (int)width / 2) * 3);
-			var y = (int)(Random.Range(0, (int)width / 2) * 3);
-			maze[x, y] = 1;
-            for(var j = 0; j<complexity; j++)
-			{
-                //I think something with these neighbors are causing severe problems. Changing their values has helped a little bit, but not by much. I tried removing it before, only to get a 100% error rate.
-                //Im going to leave them in here, despite the fact that I think these are the things causing me the most serious of problems.
-				Debug.Log("I'm going to go for: " + x + "," + y);
-				List<Point> neighbors = new List<Point>();
-				if (x > 3)
-				{
-					neighbors.Add(new Point(x - 2, y));
-				}
-				if (x < width - 4)
-				{
-					neighbors.Add(new Point(x + 2, y));
-				}
-				if (y > 3)
-				{
-					neighbors.Add(new Point(x, y - 2));
-				}
-				if (y < height - 4)
-				{
-					neighbors.Add(new Point(x, y + 2));
-				}
-
-				if (neighbors.Count > 1)
-				{
-					Point newLocation = neighbors[Random.Range(0, neighbors.Count)];
-					int xx = (int)newLocation.x;
-					int yy = (int)newLocation.y;
-
-					if (maze[xx, yy] == 0)
-					{
-						maze[xx, yy] = 1;
-						maze[(int)(xx + (x - xx) / 2), (int)(yy + (y - yy) / 2)] = 1;
-						x = xx;
-						y = yy;
-					}
-				}
-				else
-				{
-					break;
-				}
-
-				Debug.Log(this.ToString());
+				maze[i, j] = 1;
 			}
 		}
 
-		Debug.Log("Done!" + this.ToString());
+		//this code also brings nightmares. It does not spark joy. I did not feel joy typing and fiddling with this code.
+		int x = (int)(Random.Range(2, width / 2) * 2);
+		int y = (int)(Random.Range(2, height / 2) * 2);
+		//Hey guys, look! I stopped being a moron for 5 minutes and actually typed height instead of width again! this alone is an A!
+		track.Add(new Point(x + 1, y + 1));
+		maze[x, y] = 0;
+
+		while (track.Count > 0)
+		{
+			//Not this again... How much nightmare inducing code goes into this???
+			int lastTrackIndex = track.Count - 1;
+			Point check = track[lastTrackIndex];
+			x = check.x;
+			y = check.y;
+			List<Point> neighbors = new List<Point>();
+			if (x > 2 && maze[x - 2, y] == 1)
+			{ 
+				neighbors.Add(new Point(x - 2, y));
+			}
+			if (x < width - 2 && maze[x + 2, y] == 1)
+			{ 
+				neighbors.Add(new Point(x + 2, y));
+			}
+			if (y > 2 && maze[x, y - 2] == 1)
+			{ 
+				neighbors.Add(new Point(x, y - 2));
+			}
+			if (y < height - 2 && maze[x, y + 2] == 1)
+			{ 
+				neighbors.Add(new Point(x, y + 2));
+			}
+
+            if(neighbors.Count = 0)
+			{
+				track.RemoveAt(lastTrackIndex);
+			} else
+			{
+				Point newCheck = neighbors[Random.Range(0, neighbors.Count)];
+				int xx = newCheck.x;
+				int yy = newCheck.y;
+				maze[xx, yy] = 0;
+				maze[(int)(xx + (x - xx) / 2), (int)(yy + (y - yy) / 2)] = 0;
+				track.Add(newCheck);
+			}
+			Debug.Log("I'm going through...\n" + this.ToString());
+		}
+		Debug.Log("Finished!\n" + this.ToString());
 	}
 
-
-	public override string ToString()
+    public override string ToString()
 	{
 		string result = "";
-		result = "Size: " + width + "," + height + " Complexity: " + complexity + "  Density: " + density + "\n";
-		if (width > 0 && height > 0)
+		result = "Size: " + width + "," + height + "\n" + "Backtrack: ";
+        foreach(Point pt in track)
 		{
-			for (int y = 0; y < height; y++)
+			result += pt.ToString();
+		}
+		result += "\n\n";
+        if(width>0 && height > 0)
+		{
+            for(int y = 0; y<height; y++)
 			{
-				for (int x = 0; x < width; x++)
+                for(int x = 0; x<width; x++)
 				{
-					
 					result += maze[x, y];
 				}
 				result += "\n";
 			}
-		}
-		else
+			
+		} else
 		{
-			result = "A maze could not be made. Please try again."; //Shouldn't this appear instead of an error in console?
+			result = "No Maze Made. Please press the Regenerate button on screen.";
 		}
-
 		return result;
 	}
 
-	public bool IsWall(int x, int y)
+    public bool IsAWall(int x, int y)
 	{
-		if (x < 0 || x > width) { Debug.LogError("This X value is not valid, only 0 to width (" + width + ") is acceptable!"); }
-		if (y < 0 || y > height) { Debug.LogError("This Y value is not valid, only 0 to height (" + height + ") is acceptable!"); }
-        //Shouldn't these two appear instead of an error in the console?
+		if (x < 0 || x > width) { Debug.LogError("Could not generate because the X value is not valid. only 0 to width (" + width + ") is permitted."); }
+		if (y < 0 || y > height) { Debug.LogError("Could not generate because the Y value is not valid. only 0 to height (" + height + ") is permitted"); }
+
 		return (maze[x, y] == 0) ? false : true;
 	}
 }
-
-//Without a doubt, this has been the hardest Ive worked on trying to make something work properly. I hit so many roadblocks and I felt like I was getting absolutely nowhere.
-//In the end, I was able to make tweaks to the code that have stablized it a great deal, but is no where close to where I expect it to be.
-//Kinda wanted to give up sometimes. But in the end I chose to perservere, and did all I could and all I knew how to do to try and get it to a functional place. Or as functional as I could make it.
-//This is not my best work. I know that. Im looking to change that, and fix that however I can, so it meets my standards.
-// By the way... Build failed.
